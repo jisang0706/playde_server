647daafafaa02c9c2791951f834aea7109c6fe47
jisang <shinysite3@gmail.com>
2021-09-04 01:01:59 +0900

    firebaseadmin error fixing

--- main/helper/push_fcm_notification.py
+++ main/helper/push_fcm_notification.py
@@ -1,14 +1,4 @@
-from firebase_admin import messaging
-
 def send_to_firebase_cloud_messaging(registration_tocken, title, body):
 
-    message = messaging.Message(
-        notification=messaging.Notification(
-            title = title,
-            body = body,
-        ),
-        token=registration_tocken,
-    )
-
-    response = messaging.send(message)
+    response = 'temp'
     return response
